6e5c4baab79e1fb55b75932339bae8b98c9a07af
Hack Reactor Student <student@hackreactor.com>
2017-11-29 16:42:57 -0800

    started stack in function shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,79 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+  this.storage = {};
+  // this.push = function (value) {
+  //   this.storage[counter] = value;
+  //   this.counter++;
+  // };
+
+  // this.pop = function (value) {
+  //   if (this.counter === 0) {
+  //     return undefined;
+  //   }
+
+  //   this.counter--;
+  //   var deleted = this.storage[counter];
+  //   delete this.storage[counter];
+  //   return deleted;
+  // };
+
+  // this.size = function (value) {
+  //   return this.counter;
+  // };
+
+  
 };
 
-var stackMethods = {};
+
+
+var stackMethods = {
+
+  push: function (value) {
+    this.storage[counter] = value;
+    this.counter++;
+  },
+
+  pop: function (value) {
+    if (this.counter === 0) {
+      return undefined;
+    }
+
+    this.counter--;
+    var deleted = this.storage[counter];
+    delete this.storage[counter];
+    return deleted;
+  },
+
+  size: function (value) {
+    return this.counter;
+  }
+};
+
+// stackMethods.push = function (value) {
+//   this.storage[counter] = value;
+//   this.counter++;
+// };
+
+// stackMethods.pop = function (value) {
+//   if (this.counter === 0) {
+//     return undefined;
+//   }
+
+//   this.counter--;
+//   var deleted = this.storage[counter];
+//   delete this.storage[counter];
+//   return deleted;
+// };
+
+// stackMethods.size = function (value) {
+//   return this.counter;
+// };
+
+_.extend(Stack, stackMethods);
+
+
+
 
 
